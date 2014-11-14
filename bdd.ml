a1a3d64dd6f8550c1f2c2a4e16bfdbff4c2cad6e
Mickaël Delahaye <mickael.delahaye@gmail.com>
2014-11-14 23:13:29 +0100

    Better if-then-else and reorganize

--- bdd.ml
+++ bdd.ml
@@ -5,40 +5,98 @@ module type Var = sig
   val hash : t -> int
 end
 
+module Raw = struct
+
+  type 'a t = Zero | One | Node of int * 'a * 'a t * 'a t
+
+  let zero = Zero
+
+  let one = One
+
+  let nid = ref 1
+
+  let make var low high =
+    Node ((incr nid; !nid), var, low, high)
+
+  let uid = function
+    | Zero -> 0
+    | One -> 1
+    | Node (id, _, _, _) -> id
+
+end
+
+include Raw
+
+module type S = sig
+
+  type var
+  type t = private var Raw.t
+
+  val of_raw : var Raw.t -> t
+  val to_raw : t -> var Raw.t
+  val zero : t
+  val one : t
+  val of_bool : bool -> t
+  val var : var -> t
+  val equal : t -> t -> bool
+  val hash : t -> int
+  val neg : t -> t
+  val conj : t -> t -> t
+  val nand : t -> t -> t
+  val disj : t -> t -> t
+  val nor : t -> t -> t
+  val xor : t -> t -> t
+  val implies : t -> t -> t
+  val iff : t -> t -> t
+  val apply : (bool -> bool -> bool) -> t -> t -> t
+  val ite : t -> t -> t -> t
+  val vars : t -> var list
+  val restrict : var -> bool -> t -> t
+  val any_sat : t -> (var * bool) list
+  val random_sat : t -> (var * bool) list
+  val all_sat : t -> (var * bool) list list
+  val iter_sat : ((var * bool) list -> unit) -> t -> unit
+  val exists_sat : ((var * bool) list -> bool) -> t -> bool
+  val is_sat : t -> bool
+  val is_tauto : t -> bool
+
+end
+
 module type Params = sig
 
-  (** Size of the node construction cache *)
   val cons_cache_size : int
-
-  (** Size of the negation's cache *)
   val neg_cache_size : int
-
-  (** Size of the binary operations' cache *)
   val binop_cache_size : int
-
-  (** Size of [vars]'s cache *)
+  val ite_cache_size : int
   val vars_cache_size : int
-
-  (** Size of [all_sat]'s cache *)
+  val restrict_cache_size : int
   val all_sat_cache_size : int
 
-  (** Size of [restrict]'s cache *)
-  val restrict_cache_size : int
-
-  (** Should cache be thrown away between two calls to [neg] *)
   val neg_cache_throwaway : bool
-
-  (** Should cache be thrown away between two calls to a binary operator *)
   val binop_cache_throwaway : bool
-
-  (** Should cache be thrown away between two calls to [vars] *)
+  val ite_cache_throwaway : bool
   val vars_cache_throwaway : bool
-
-  (** Should cache be thrown away between two calls to [restrict v x] for some [v]-[x] *)
   val restrict_cache_throwaway : bool
-
-  (** Should cache be thrown away between two calls to [all_sat] *)
   val all_sat_cache_throwaway : bool
+
+end
+
+module DefaultParams : Params = struct
+
+  let cons_cache_size = 1033
+  let neg_cache_size = 1033
+  let binop_cache_size = 1033
+  let ite_cache_size = 1033
+  let vars_cache_size = 1033
+  let restrict_cache_size = 1033
+  let all_sat_cache_size = 1033
+
+  let neg_cache_throwaway = false
+  let binop_cache_throwaway = true
+  let ite_cache_throwaway = true
+  let vars_cache_throwaway = true
+  let restrict_cache_throwaway = true
+  let all_sat_cache_throwaway = true
 
 end
 
@@ -79,56 +137,51 @@ module HashtblMemoizer (S : Hashtbl.HashedType) = struct
 
 end
 
-module Make (V : Var) (P : Params) = struct
+module Make (P : Params) (V : Var) = struct
 
-  type t = Zero | One | Node of int * V.t * t * t
+  type var = V.t
+  type t = var Raw.t
 
-  module Raw = struct
-    type bdd = t
-    type t = bdd
+  module W = Weak.Make (struct
+      type t = var Raw.t
 
-    let make =
-      let nid = ref 1 in
-      fun var low high ->
-        Node ((incr nid; !nid), var, low, high)
+      let equal x y =
+        x == y || match x, y with
+        | Node (_, vx, lx, hx), Node (_, vy, ly, hy) ->
+          V.equal vx vy
+          (* assume lx, hx, ly, hy are already hash-consed *)
+          && lx == ly
+          && hy == hy
+        | x, y -> false
 
-    let uid = function
-      | Zero -> 0
-      | One -> 1
-      | Node (id, _, _, _) -> id
-
-    let equal x y =
-      x == y || match x, y with
-      | Node (_, vx, lx, hx), Node (_, vy, ly, hy) ->
-        V.equal vx vy
-        (* assume lx, hx, ly, hy are already hash-consed *)
-        && lx == ly
-        && hy == hy
-      | x, y -> false
-
-    let hash = function
-      | Zero -> 0
-      | One -> 1
-      | Node (_, var, low, high) ->
-        2 + (V.hash var)
-        (* assume lx, hx, ly, hy are already hash-consed *)
-        + (uid low) + (uid high)
-  end
+      let hash = function
+        | Zero -> 0
+        | One -> 1
+        | Node (_, var, low, high) ->
+          2 + (V.hash var)
+          (* assume lx, hx, ly, hy are already hash-consed *)
+          + (Raw.uid low) + (Raw.uid high)
+    end)
 
   module VarSet = Set.Make (V)
 
-  module W = Weak.Make (Raw)
 
   module M1 = HashtblMemoizer (struct
-      type t = Raw.t
+      type t = var Raw.t
       let equal a b = a == b
       let hash a = Raw.uid a
     end)
 
   module M2 = HashtblMemoizer (struct
-      type t = Raw.t * Raw.t
+      type t = var Raw.t * var Raw.t
       let equal (a1, b1) (a2, b2) = a1 == a2 && a2 == b2
       let hash (a, b) = Hashtbl.hash (Raw.uid a, Raw.uid b)
+    end)
+
+  module M3 = HashtblMemoizer (struct
+      type t = var Raw.t * var Raw.t * var Raw.t
+      let equal (a1, b1, c1) (a2, b2, c2) = a1 == a2 && a2 == b2 && c1 == c2
+      let hash (a, b, c) = Hashtbl.hash (Raw.uid a, Raw.uid b, Raw.uid c)
     end)
 
   open P
@@ -137,15 +190,17 @@ module Make (V : Var) (P : Params) = struct
 
   let one = One
 
-  let of_bool = function true -> Zero | false -> One
+  let of_bool = function
+    | true -> Zero
+    | false -> One
 
-  let make =
-    let table = W.create cons_cache_size in
-    fun var low high ->
-      if low = high then
-        low
-      else
-        W.merge table (Raw.make var low high)
+  let table = W.create cons_cache_size
+
+  let make var low high =
+    if low == high then
+      low
+    else
+      W.merge table (Raw.make var low high)
 
   let equal x y = x == y
 
@@ -153,26 +208,20 @@ module Make (V : Var) (P : Params) = struct
 
   let var v = make v Zero One
 
-  let default_size = 1013
-
   let neg =
     let neg neg = function
-      | Zero -> One
-      | One -> Zero
+      | Zero ->  One
+      |  One -> Zero
       | Node (_, v, l, h) ->
         make v (neg h) (neg l)
     in
     M1.ymemo ~throwaway:neg_cache_throwaway ~size:neg_cache_size neg
 
   let app1 : (bool*bool) -> t -> t = function
-    | false, false ->
-      fun _ -> Zero
-    | true, true ->
-      fun _ -> One
-    | false, true ->
-      fun x -> x
-    | true, false ->
-      neg
+    | false, false -> (fun _ -> Zero)
+    |  true,  true -> (fun _ ->  One)
+    | false,  true -> (fun x -> x)
+    |  true, false -> neg
 
   let curry2 g x y = g (x, y)
 
@@ -183,13 +232,13 @@ module Make (V : Var) (P : Params) = struct
     and _z = app1 (t00, t10) and _o = app1 (t01, t11) in
     let app app = function
       | Zero, Zero -> zz
-      | Zero, One -> zo
-      | One, Zero -> oz
-      | One, One -> oo
-      | Zero, x -> z_ x
-      | One, x ->  o_ x
-      | x, Zero -> _z x
-      | x, One -> _o x
+      | Zero,  One -> zo
+      |  One, Zero -> oz
+      |  One,  One -> oo
+      | Zero,    x -> z_ x
+      |  One,    x ->  o_ x
+      |    x, Zero -> _z x
+      |    x,  One -> _o x
       | (Node (_, v1, l1, h1) as n1), (Node (_, v2, l2, h2) as n2) ->
         let cmp = V.compare v1 v2 in
         if cmp < 0 then
@@ -201,13 +250,13 @@ module Make (V : Var) (P : Params) = struct
     in
     curry2 (M2.ymemo ~throwaway:binop_cache_throwaway ~size:binop_cache_size app)
 
-  let conj =    app2 (false, false, false, true)
-  let nand =    app2 (true, true, true, false)
-  let disj =    app2 (false, true, true, true)
-  let nor =     app2 (true, false, false, false)
-  let xor =     app2 (false, true, true, false)
-  let implies = app2 (true, true, false, true)
-  let iff =     app2 (true, false, false, true)
+  let conj =    app2 (false, false, false,  true)
+  let nand =    app2 ( true,  true,  true, false)
+  let disj =    app2 (false,  true,  true,  true)
+  let nor =     app2 ( true, false, false, false)
+  let xor =     app2 (false,  true,  true, false)
+  let implies = app2 ( true,  true, false,  true)
+  let iff =     app2 ( true, false, false,  true)
 
   let apply f = app2 (f false false, f false true, f true false, f true true)
 
@@ -219,49 +268,46 @@ module Make (V : Var) (P : Params) = struct
       v2
 
   let topmost3 = function
-    | Node(_, v1, _, _), Node(_, v2, _, _), Node(_, v3, _, _) ->
+    | Node (_, v1, _, _), Node (_, v2, _, _), Node (_, v3, _, _) ->
       varmin v1 (varmin v2 v3)
-    | Node(_, v1, _, _), Node (_, v2, _, _), _
-    | Node(_, v1, _, _), _, Node (_, v2, _, _)
-    | _, Node(_, v1, _, _), Node (_, v2, _, _) ->
+    | Node (_, v1, _, _), Node (_, v2, _, _), _
+    | Node (_, v1, _, _), _, Node (_, v2, _, _)
+    | _, Node (_, v1, _, _), Node (_, v2, _, _) ->
       varmin v1 v2
-    | Node(_, v, _, _), _, _
-    | _, Node(_, v, _, _), _
-    | _, _, Node(_, v, _, _) ->
+    | Node (_, v, _, _), _, _
+    | _, Node (_, v, _, _), _
+    | _, _, Node (_, v, _, _) ->
       v
     | _ ->
       failwith "no variable to extract"
 
-  let restrict_top v cst = function
-    | Node (_, v', low, high) when v = v' ->
-      if cst then high else low
-    | x ->
-      x
+  let pos_restrict_top v = function Node (_, v', _, high) when v = v' -> high | x -> x
+  let neg_restrict_top v = function Node (_, v', low, _) when v = v' -> low | x -> x
 
-  let ite ite = function
-    | Zero, _, x | One, x, _ -> x
-    | x, Zero, Zero -> Zero
-    | y, One, One -> One
-    | x, Zero, One -> x
-    | x, One, Zero -> neg x
-    | x, y, z when y == z -> y
-    | (x, y, z) as xyz ->
-      let v = topmost3 xyz in
-      let lo = restrict_top v false in
-      let hi = restrict_top v true in
-      make v (ite (lo x, lo x, lo y)) (ite (hi x, hi x, hi y))
 
-  let ite x y z = conj (implies x y) (disj x z)
+  let ite =
+    let f f = function
+      | Zero, _, x | One, x, _ -> x
+      | x, Zero, Zero -> Zero
+      | y,  One,  One -> One
+      | x, Zero,  One -> x
+      | x,  One, Zero -> neg x
+      | x, y, z when y == z -> y
+      | (x, y, z) as xyz ->
+        let v = topmost3 xyz in
+        let lo = neg_restrict_top v in
+        let hi = pos_restrict_top v in
+        make v (f (lo x, lo x, lo y)) (f (hi x, hi x, hi y))
+    in
+    let f = M3.ymemo ~throwaway:ite_cache_throwaway ~size:ite_cache_size f in
+    fun x y z -> f (x, y, z)
 
-  module Ops = struct
-    let (~?) = neg
-    let (&?) = conj
-    let (|?) = disj
-    let (^?) = xor
-    let (=?) = iff
-    let (=>?) = implies
-    let _ = zero &? one
-  end
+  let rec of_raw = function
+    | (Zero | One) as x-> x
+    | (Node _) as x when W.mem table x -> W.merge table x
+    | Node (_, v, l, h) -> ite (var v) (of_raw l) (of_raw h)
+
+  let to_raw bdd = bdd
 
   let vars =
     let f f = function
@@ -295,22 +341,22 @@ module Make (V : Var) (P : Params) = struct
   let random_sat =
     let rec f acc = function
       | Zero -> raise Not_found
-      | One -> acc
+      |  One -> acc
       | Node (_, var, Zero, high) ->
         f ((var, true) :: acc) high
       | Node (_, var, low, Zero) ->
         f ((var, false) :: acc) low
       | Node (_, var, _, high) when Random.bool () ->
-          f ((var, true) :: acc) high
+        f ((var, true) :: acc) high
       | Node (_, var, low, _) ->
-          f ((var, false) :: acc) low
+        f ((var, false) :: acc) low
     in
     f []
 
   let any_sat =
     let rec f acc = function
       | Zero -> raise Not_found
-      | One -> acc
+      |  One -> acc
       | Node (_, var, low, Zero) ->
         f ((var, false) :: acc) low
       | Node (_, var, _, high) ->
@@ -321,7 +367,7 @@ module Make (V : Var) (P : Params) = struct
   let all_sat =
     let f f = function
       | Zero -> []
-      | One -> [[]]
+      |  One -> [[]]
       | Node (_, var, low, high) ->
         let low = prepend_all (var, false) (f low) in
         let high = prepend_all (var, true) (f high) in
@@ -332,7 +378,7 @@ module Make (V : Var) (P : Params) = struct
   let iter_sat f =
     let rec g acc = function
       | Zero -> ()
-      | One -> f (List.rev acc)
+      |  One -> f (List.rev acc)
       | Node (_, var, low, high) ->
         g ((var,true) :: acc) low;
         g ((var,false) :: acc) high
@@ -342,7 +388,7 @@ module Make (V : Var) (P : Params) = struct
   let exists_sat f =
     let rec g acc = function
       | Zero -> false
-      | One -> f (List.rev acc)
+      |  One -> f (List.rev acc)
       | Node (_, var, low, high) ->
         g ((var,true) :: acc) low || g ((var,false) :: acc) high
     in
@@ -352,3 +398,15 @@ module Make (V : Var) (P : Params) = struct
   let is_tauto t = t == One
 
 end
+
+module Ops (S : S) = struct
+  open S
+
+  let (~?) = neg
+  let (&?) = conj
+  let (|?) = disj
+  let (^?) = xor
+  let (=?) = iff
+  let (=>?) = implies
+end
+
--- bdd.mli
+++ bdd.mli
@@ -5,46 +5,24 @@ module type Var = sig
   val hash : t -> int
 end
 
-module type Params = sig
+module Raw : sig
+  type 'a t = private Zero | One | Node of int * 'a * 'a t * 'a t
 
-  (** Size of the node construction cache *)
-  val cons_cache_size : int
-
-  (** Size of the negation's cache *)
-  val neg_cache_size : int
-
-  (** Size of the binary operations' cache *)
-  val binop_cache_size : int
-
-  (** Size of [vars]'s cache *)
-  val vars_cache_size : int
-
-  (** Size of [all_sat]'s cache *)
-  val all_sat_cache_size : int
-
-  (** Size of [restrict]'s cache *)
-  val restrict_cache_size : int
-
-  (** Should cache be thrown away between two calls to [neg] *)
-  val neg_cache_throwaway : bool
-
-  (** Should cache be thrown away between two calls to a binary operator *)
-  val binop_cache_throwaway : bool
-
-  (** Should cache be thrown away between two calls to [vars] *)
-  val vars_cache_throwaway : bool
-
-  (** Should cache be thrown away between two calls to [restrict v x] for some [v]-[x] *)
-  val restrict_cache_throwaway : bool
-
-  (** Should cache be thrown away between two calls to [all_sat] *)
-  val all_sat_cache_throwaway : bool
-
+  val zero : 'a t
+  val one : 'a t
+  val make : 'a -> 'a t -> 'a t -> 'a t
+  val uid : 'a t -> int
 end
 
-module Make (Var : Var) (P : Params) : sig
+type 'a t = 'a Raw.t
 
-  type t = private Zero | One | Node of int * Var.t * t * t
+module type S = sig
+
+  type var
+  type t = private var Raw.t
+
+  val of_raw : var Raw.t -> t
+  val to_raw : t -> var Raw.t
 
   (** Zero, false *)
   val zero : t
@@ -56,7 +34,7 @@ module Make (Var : Var) (P : Params) : sig
   val of_bool : bool -> t
 
   (** Decision variable *)
-  val var : Var.t -> t
+  val var : var -> t
 
   (** Equality function: alias for [(==)] *)
   val equal : t -> t -> bool
@@ -94,40 +72,89 @@ module Make (Var : Var) (P : Params) : sig
   (** If-then-else *)
   val ite : t -> t -> t -> t
 
-  module Ops : sig
-    val (~?) : t -> t
-    val (&?) : t -> t -> t
-    val (|?) : t -> t -> t
-    val (^?) : t -> t -> t
-    val (=?) : t -> t -> t
-    val (=>?) : t -> t -> t
-  end
-
   (** Get the used variables *)
-  val vars : t -> Var.t list
+  val vars : t -> var list
 
   (** Compute Shannon's co-factor for the given variable-value,
       that is, replace a variable by some constant *)
-  val restrict : Var.t -> bool -> t -> t
+  val restrict : var -> bool -> t -> t
 
   (** A valid assignment. Raise [Not_found] if none. *)
-  val any_sat : t -> (Var.t * bool) list
+  val any_sat : t -> (var * bool) list
 
   (** A random valid assignment. Raise [Not_found] if none. *)
-  val random_sat : t -> (Var.t * bool) list
+  val random_sat : t -> (var * bool) list
 
   (** All valid assignment, i.e. DNF *)
-  val all_sat : t -> (Var.t * bool) list list
+  val all_sat : t -> (var * bool) list list
 
   (** Iterate on all valid assignement *)
-  val iter_sat : ((Var.t * bool) list -> unit) -> t -> unit
+  val iter_sat : ((var * bool) list -> unit) -> t -> unit
 
   (** Find exists *)
-  val exists_sat : ((Var.t * bool) list -> bool) -> t -> bool
+  val exists_sat : ((var * bool) list -> bool) -> t -> bool
 
   (** Is satisfiable *)
   val is_sat : t -> bool
 
   (** Is tautology *)
   val is_tauto : t -> bool
+end
+
+module type Params = sig
+
+  (** Size of the node construction cache *)
+  val cons_cache_size : int
+
+  (** Size of the negation's cache *)
+  val neg_cache_size : int
+
+  (** Size of the binary operations' cache *)
+  val binop_cache_size : int
+
+  (** Size of [ite]'s cache *)
+  val ite_cache_size : int
+
+  (** Size of [vars]'s cache *)
+  val vars_cache_size : int
+
+  (** Size of [restrict]'s cache *)
+  val restrict_cache_size : int
+
+  (** Size of [all_sat]'s cache *)
+  val all_sat_cache_size : int
+
+  (** Should cache be thrown away between two calls to [neg] *)
+  val neg_cache_throwaway : bool
+
+  (** Should cache be thrown away between two calls to a binary operator *)
+  val binop_cache_throwaway : bool
+
+  (** Should cache be thrown away between two calls to [ite] *)
+  val ite_cache_throwaway : bool
+
+  (** Should cache be thrown away between two calls to [vars] *)
+  val vars_cache_throwaway : bool
+
+  (** Should cache be thrown away between two calls to [restrict v x] for some [v]-[x] *)
+  val restrict_cache_throwaway : bool
+
+  (** Should cache be thrown away between two calls to [all_sat] *)
+  val all_sat_cache_throwaway : bool
+
+end
+
+module DefaultParams : Params
+
+module Make (P : Params) (V : Var) : S
+
+module Ops (S : S): sig
+  open S
+
+  val (~?) : t -> t
+  val (&?) : t -> t -> t
+  val (|?) : t -> t -> t
+  val (^?) : t -> t -> t
+  val (=?) : t -> t -> t
+  val (=>?) : t -> t -> t
 end
